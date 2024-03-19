a336a12cbafc78eb813934efc57d9ecea5c1d698
lex-neufeld <lex.neufeld@gmail.com>
2024-03-19 11:52:59 +0100

    CustomerView writes customers to XML cleaned up a comment from module.info

---
+++ src/view/CustomerView.java
@@ -0,0 +1,52 @@
+package view;
+
+import controller.XMLController;
+import model.CustomerModel;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class CustomerView extends JFrame{
+    private JLabel nameLabel;
+    private JLabel dateOfBirthLabel;
+    private JLabel addressLabel;
+    private JTextField nameTextField;
+    private JTextField dateOfBirthTextField;
+    private JTextField addressTextField;
+    private JButton saveButton;
+    private CustomerModel newCustomer;
+    private final XMLController myXMLController = new XMLController();
+
+    public CustomerView() {
+
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //get parameters from form
+                String name = nameTextField.getText();
+                SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+                Date dayOfBirth = null;
+                try {
+                    dayOfBirth = formatter.parse(dateOfBirthTextField.getText());
+                } catch (ParseException ex) {
+                    throw new RuntimeException(ex);
+                }
+                Date registeredAt = new Date();
+                String address = addressTextField.getText();
+
+                //create CustomerModel
+                newCustomer = new CustomerModel(name, dayOfBirth, registeredAt, address);
+
+                //send newCustomer to XMLController
+                myXMLController.writeCustomerToList(newCustomer);
+
+            }
+        });
+    }
+}
